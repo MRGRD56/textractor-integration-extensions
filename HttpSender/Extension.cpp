da336c7443da6464165c152c0a7203a2939e553c
k.golikov <kgolikov@37bytes.com>
2022-11-24 21:19:16 +0500

    erwe

--- HttpSender/Extension.cpp
+++ HttpSender/Extension.cpp
@@ -1,39 +1,136 @@
 #include "Extension.h"
 #include <cpr/cpr.h>
 #include <nlohmann/json.hpp>
+#include <codecvt>
+#include <string>
+#include <process.h>
+#include "ShellAPI.h"
 using json = nlohmann::json;
 
+const std::string config_file_name = "HttpSenderConfig.json";
+
+const char* default_config = R"""({
+  "$info": "Learn more: https://github.com/MRGRD56/MgTextractorExtensions",
+  "sentence": {
+    "enabled": false,
+    "url": "http://localhost:8080",
+    "requestType": "JSON_TEXT",
+    "selectedThreadOnly": true
+  }
+})""";
+
+json config = nullptr;
+
+bool is_enabled() {
+	return config == nullptr || !config.is_object() || !config["sentence"]["enabled"];
+}
+
+std::string wstring_to_utf8(const std::wstring& str) {
+	std::wstring_convert<std::codecvt_utf8<wchar_t>> convert;
+	return convert.to_bytes(str);
+}
+
+std::wstring utf8_to_wstring(const std::string& str)
+{
+	std::wstring_convert<std::codecvt_utf8<wchar_t>> convert;
+	return convert.from_bytes(str);
+}
+
+inline bool file_exists(const std::string& name) {
+	struct stat buffer;
+	return stat(name.c_str(), &buffer) == 0;
+}
+
+bool has_config() {
+	return file_exists(config_file_name);
+}
+
+json get_config() {
+	const std::ifstream config_file(config_file_name);
+	if (!config_file.good()) {
+		return nullptr;
+	}
+
+	return json::parse(config_file);
+}
+
+bool check_config(const bool initialize) {
+	if (has_config()) {
+		return true;
+	}
+	
+	if (initialize) {
+		std::ofstream config_file(config_file_name);
+		config_file << default_config << std::endl;
+		config_file.close();
+
+		ShellExecute(0, 0, utf8_to_wstring(config_file_name).c_str(), 0, 0 , SW_SHOW);
+	}
+	
+	return false;
+}
+
+void update_config(const bool initialize = false) {
+	if (check_config(initialize)) {
+		config = get_config();
+	} else {
+		config = nullptr;
+	}
+}
+
 BOOL WINAPI DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
-	//switch (ul_reason_for_call)
-	//{
-	//case DLL_PROCESS_ATTACH:
-	//	MessageBoxW(NULL, L"Extension Loaded", L"Example", MB_OK);
-	//	break;
-	//case DLL_PROCESS_DETACH:
-	//	MessageBoxW(NULL, L"Extension Removed", L"Example", MB_OK);
-	//	break;
-	//}
+	switch (ul_reason_for_call) {
+	case DLL_PROCESS_ATTACH:
+		update_config(true);
+		// MessageBoxW(NULL, L"Extension Loaded", L"Example", MB_OK);
+		break;
+	case DLL_PROCESS_DETACH:
+		// MessageBoxW(NULL, L"Extension Removed", L"Example", MB_OK);
+		break;
+	}
 	return TRUE;
 }
 
-void SendHttpRequestAsync(std::wstring& sentence) {
-	//json requestBodyJson = {
-	//	{"text", *sentence.c_str()},
-	//	{"sentenceInfo", {
-	//		
-	//	}}
-	//};
+void send_http_request_async(std::wstring& sentence, SentenceInfo sentenceInfo) {
+	const std::string sentence_string = wstring_to_utf8(sentence);
+	
+	std::string request_body;
+	std::string content_type;
 
+	const json sentence_config = config["sentence"];
+	const std::string request_type = sentence_config["requestType"].get<std::string>();
 
-	std::string sentenceString;
-	std::transform(sentence.begin(), sentence.end(), std::back_inserter(sentenceString), [](wchar_t c) {
-		return (char)c;
-		});
+	json request_body_json;
+	if (request_type == "JSON_TEXT") {
+		request_body_json = {
+			{"text", *sentence.c_str()}
+		};
+		request_body = request_body_json.dump();
+		content_type = "application/json";
+	} else if (request_type == "JSON_TEXT_WITH_META") {
+		request_body_json = {
+			{"text", *sentence.c_str()},
+			{"meta", {
+						{"isCurrentSelect", static_cast<bool>(sentenceInfo["current select"])},
+						{"processId", sentenceInfo["process id"]},
+						{"threadNumber", sentenceInfo["text number"]},
+						{"threadName", sentenceInfo["text name"]}
+			}}
+		};
+		request_body = request_body_json.dump();
+		content_type = "application/json";
+	} else {
+		request_body = sentence_string;
+		content_type = "text/plain";
+	}
 
-	cpr::AsyncResponse response = cpr::PostAsync(
+	cpr::AsyncResponse response = PostAsync(
 		cpr::Url{ "http://localhost:1234/sentence" },
-		cpr::Body{ sentenceString },
-		cpr::Header{ {"Content-Type", "text/plain"} });
+		cpr::Body{ request_body },
+		cpr::Header{
+			{"Content-Type", content_type},
+			{"Connection", "Keep-Alive"}
+		});
 }
 
 /*
@@ -46,8 +143,16 @@ void SendHttpRequestAsync(std::wstring& sentence) {
 	It will not be run concurrently with DllMain.
 */
 bool ProcessSentence(std::wstring& sentence, SentenceInfo sentenceInfo) {
-	if (sentenceInfo["current select"]) {
-		SendHttpRequestAsync(sentence);
+	update_config();
+	
+	if (!is_enabled()) {
+		return false;
+	}
+
+	const json sentence_config = config["sentence"];
+	
+	if (!sentence_config["selectedThreadOnly"] || sentenceInfo["current select"]) {
+		send_http_request_async(sentence, sentenceInfo);
 	}
 
 	return false;
---
+++ HttpSender/README.md
@@ -0,0 +1,69 @@
+﻿### Configuration
+
+#### HttpSenderConfig.json
+
+##### Example
+```json
+{
+  "sentence": {
+    "enabled": false,
+    "url": "http://localhost:9650/sentence",
+    "requestType": "PLAIN_TEXT",
+    "selectedThreadOnly": true
+  }
+}
+```
+
+| Field                | Type                                                               |
+|----------------------|--------------------------------------------------------------------|
+| `enabled`*           | boolean                                                            |
+| `url`                | string                                                             |
+| `requestType`**      | `"PLAIN_TEXT"` &vert; `"JSON_TEXT"` &vert; `"JSON_TEXT_WITH_META"` |
+| `selectedThreadOnly` | boolean                                                            |
+
+**⚠️⚠️⚠️&ast;Set `enabled` field to `true` to make it work**
+
+##### **`requestType` field
+
+###### PLAIN_TEXT
+
+```http request
+POST http://localhost:9650/sentence
+Content-Type: text/plain
+
+Lorem ipsum dolor sit amet
+```
+
+###### JSON_TEXT
+
+```http request
+POST http://localhost:9650/sentence
+Content-Type: application/json
+
+{
+  "text": "Lorem ipsum dolor sit amet"
+}
+```
+
+###### JSON_TEXT_WITH_META
+
+```http request
+POST http://localhost:9650/sentence
+Content-Type: application/json
+
+{
+  "text": "Lorem ipsum dolor sit amet",
+  "meta": {
+    "isCurrentSelect": true,    // sentenceInfo["current select"]
+    "processId": 2898235,       // sentenceInfo["process id"]
+    "threadNumber": 12,         // sentenceInfo["text number"]
+    "threadName": "Some name"   // sentenceInfo["text name"]
+  }
+}
+```
+
+| Value                   | Explanation |
+|-------------------------|-------------|
+| `"PLAIN_TEXT"`          |             |
+| `"JSON_TEXT"`           |             |
+| `"JSON_TEXT_WITH_META"` |             |
